1245b6c9d556d272b6f038477c42fd8300bbe1e1
joaopmerlin <joao.p.merlin@gmail.com>
2017-05-08 19:43:35 -0300

    create project base create config server

---
+++ config-server/src/main/java/br/com/isis/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.isis.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
