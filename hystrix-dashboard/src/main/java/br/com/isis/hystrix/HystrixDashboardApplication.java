a28ccaa918a43acfc6d85ef8861d293e38c0cf13
joaopmerlin <joao.p.merlin@gmail.com>
2017-05-08 21:25:44 -0300

    create project for hystrix dashboard

---
+++ hystrix-dashboard/src/main/java/br/com/isis/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,24 @@
+package br.com.isis.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@Controller
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+}
