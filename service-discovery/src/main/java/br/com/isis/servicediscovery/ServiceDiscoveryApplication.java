2b8f40f3949099bcbe3af6338fcb4520f65eec65
Gaspar Barancelli Junior <gasparbarancelli@gmail.com>
2017-05-10 22:18:31 -0300

    Move swagger config for other class

--- service-discovery/src/main/java/br/com/isis/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/br/com/isis/servicediscovery/ServiceDiscoveryApplication.java
@@ -4,21 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableOAuth2Sso
-@EnableSwagger2
 public class ServiceDiscoveryApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
@@ -37,23 +28,4 @@ public class ServiceDiscoveryApplication extends WebSecurityConfigurerAdapter {
 		// @formatter:on
 	}
 
-	@Bean
-	public Docket newsApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("ProjectIsis")
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build()
-				.pathMapping("/");
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Service Discovery with Swagger")
-				.description("Documentation of all the end points for the discovery of the service")
-				.version("1.0")
-				.build();
-	}
 }
