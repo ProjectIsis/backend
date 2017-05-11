2734b7c6015f01698a4122488915c9cd1c84b7af
Gaspar Barancelli Junior <gasparbarancelli@gmail.com>
2017-05-10 22:04:39 -0300

    Swagger configuration

--- service-discovery/src/main/java/br/com/isis/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/br/com/isis/servicediscovery/ServiceDiscoveryApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableOAuth2Sso
+@EnableSwagger2
 public class ServiceDiscoveryApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
@@ -26,5 +35,25 @@ public class ServiceDiscoveryApplication extends WebSecurityConfigurerAdapter {
 				.anyRequest().permitAll()
 				.and().csrf().disable();
 		// @formatter:on
+	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("ProjectIsis")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Service Discovery with Swagger")
+				.description("Documentation of all the end points for the discovery of the service")
+				.version("1.0")
+				.build();
 	}
 }
